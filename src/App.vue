52033a89082552bc3a4b745ca159bddb9c6dba5b
Grace <grace.mw43@gmail.com>
2025-06-13 16:20:57 +0700

    [update] เพิ่ม props เพื่อจำแนก date time picker แต่ละแบบ ใน date time picker component ,เรียก date picker component ใน HomeView

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,11 @@
 <template>
   <v-app>
-    <v-container :fluid="true" class="pa-0">
-      <DatePicker />
-    </v-container>
+    <v-container :fluid="true" class="pa-0"> <router-view /> </v-container>
   </v-app>
 </template>
 s
 <script setup lang="ts">
-// import { ref } from 'vue';
-import DatePicker from './components/DatePicker.vue';
-
-// const isOpenDatePicker = ref(false);
-// const dateTimePick = ref('');
+import { RouterView } from 'vue-router';
 </script>
 
 <style></style>
---
+++ src/components/DateTimePicker.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container :fluid="true" class="pa-0">
+    <DatePickerMilitaryTime v-if="props.timeType === 'militaryTime'" />
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import DatePickerMilitaryTime from './DatePickerMilitaryTime.vue';
+
+const props = defineProps({
+  timeType: String
+});
+</script>
+
+<style scoped></style>
